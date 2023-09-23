e7f30b02ee7e0e50b9e0ec6988aa1c93fb0a2c3b
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-09-23 12:19:35 +0300

    Create cinema_ticket.py

---
+++ Python-Programming-Basics/Labs-And-Homeworks/cinema_ticket.py
@@ -0,0 +1,18 @@
+day_of_week = input()
+
+if day_of_week == "Monday":
+    print("12")
+elif day_of_week == "Tuesday":
+    print("12")
+elif day_of_week == "Wednesday":
+    print("14")
+elif day_of_week == "Thursday":
+    print("14")
+elif day_of_week == "Friday":
+    print("12")
+elif day_of_week == "Saturday":
+    print("16")
+elif day_of_week == "Sunday":
+    print("16")
+else:
+    print("")
