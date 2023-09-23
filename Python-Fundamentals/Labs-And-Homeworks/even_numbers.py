b3c8b7c784f7d07f025e16e17fcf6b05f45f20cf
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-09-23 15:36:26 +0300

    Create even_numbers.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/even_numbers.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for _ in range(n):
+    current_number = int(input())
+
+    if current_number % 2 != 0:
+        print(f"{current_number} is odd!")
+        break
+else:
+    print("All numbers are even.")
