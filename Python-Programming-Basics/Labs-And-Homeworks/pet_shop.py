003b3ed3b9ec700e8f19aa7855c4283ca839fb1d
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-09-23 11:13:21 +0300

    Create pet_shop.py

---
+++ Python-Programming-Basics/Labs-And-Homeworks/pet_shop.py
@@ -0,0 +1,9 @@
+num_food_dogs = int(input())
+num_food_cats = int(input())
+
+money_dogs = num_food_dogs * 2.50
+money_cats = num_food_cats * 4
+
+price = money_dogs + money_cats
+
+print(f"{price} lv.")
