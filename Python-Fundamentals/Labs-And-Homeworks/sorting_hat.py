06cedd7f09f5d5cc9867e32fa790e89a58d16aff
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-09-23 15:33:05 +0300

    Create sorting_hat.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/sorting_hat.py
@@ -0,0 +1,22 @@
+while True:
+    name = input()
+
+    if name == "Welcome!":
+        print("Welcome to Hogwarts.")
+        break
+
+    if name == "Voldemort":
+        print("You must not speak of that name!")
+        break
+
+    house = ""
+    if len(name) < 5:
+        house = "Gryffindor"
+    elif len(name) == 5:
+        house = "Slytherin"
+    elif len(name) == 6:
+        house = "Ravenclaw"
+    else:
+        house = "Hufflepuff"
+
+    print(f"{name} goes to {house}.")
