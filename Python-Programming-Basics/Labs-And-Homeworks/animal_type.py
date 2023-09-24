caeb2e8e505fee73ab2826c02736c8e66ed33598
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-09-24 11:31:55 +0300

    Update animal_type.py

--- Python-Programming-Basics/Labs-And-Homeworks/animal_type.py
+++ Python-Programming-Basics/Labs-And-Homeworks/animal_type.py
@@ -1,37 +1,12 @@
-degrees = int(input())
-time_of_day = input()
+type_animal = input()
 
-outfit = ""
-shoes = ""
-if time_of_day == "Morning":
-    if 10 <= degrees <= 18:
-        outfit = "Sweatshirt"
-        shoes = "Sneakers"
-    elif 18 < degrees <= 24:
-        outfit = "Shirt"
-        shoes = "Moccasins"
-    elif degrees > 24:
-        outfit = "T-Shirt"
-        shoes = "Sandals"
-elif time_of_day == "Afternoon":
-    if 10 <= degrees <= 18:
-        outfit = "Shirt"
-        shoes = "Moccasins"
-    elif 18 < degrees <= 24:
-        outfit = "T-Shirt"
-        shoes = "Sandals"
-    elif degrees > 24:
-        outfit = "Swim Suit"
-        shoes = "Barefoot"
-elif time_of_day == "Evening":
-    if 10 <= degrees <= 18:
-        outfit = "Shirt"
-        shoes = "Moccasins"
-    elif 18 < degrees <= 24:
-        outfit = "Shirt"
-        shoes = "Moccasins"
-    elif degrees > 24:
-        outfit = "Shirt"
-        shoes = "Moccasins"
-
-print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
+if type_animal == "dog":
+    print("mammal")
+elif type_animal == "crocodile":
+    print("reptile")
+elif type_animal == "tortoise":
+    print("reptile")
+elif type_animal == "snake":
+    print("reptile")
+else:
+    print("unknown")
