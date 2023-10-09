fcb6ed9b8888db548df0deab50112171909acba4
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-09 10:57:39 +0300

    Update orders.py

--- Python-Fundamentals/Labs-And-Homeworks/Basic-Syntax-Conditional-Statements-and-Loops/orders.py
+++ Python-Fundamentals/Labs-And-Homeworks/Basic-Syntax-Conditional-Statements-and-Loops/orders.py
@@ -1,12 +1,14 @@
-first_string = input()
-second_string = input()
-last_printed_string = first_string
+n = int(input())
+total_price = 0
 
-for character_index in range(len(first_string)):
-    left_part = second_string[:character_index + 1]
-    right_part = first_string[character_index + 1:]
-    new_string = left_part + right_part
+for _ in range(n):
+    price_per_capsule = float(input())
+    days = int(input())
+    capsules_per_day = int(input())
 
-    if new_string != last_printed_string:
-        print(new_string)
-        last_printed_string = new_string
+    if 0.01 <= price_per_capsule <= 100.00 and 1 <= days <= 31 and 1 <= capsules_per_day <= 2000:
+        order_price = price_per_capsule * days * capsules_per_day
+        total_price += order_price
+        print(f"The price for the coffee is: ${order_price:.2f}")
+
+print(f"Total: ${total_price:.2f}")
