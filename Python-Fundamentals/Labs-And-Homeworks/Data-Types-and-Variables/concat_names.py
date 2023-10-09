0610ee509382dcfb19bc67bf3f9a10ebe73c1e5e
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-09 12:27:41 +0300

    Delete Python-Fundamentals/Labs-And-Homeworks/Data-Types-and-Variables directory

--- Python-Fundamentals/Labs-And-Homeworks/Data-Types-and-Variables/concat_names.py
+++
@@ -1,5 +0,0 @@
-first_name = input()
-second_name = input()
-delimiter = input()
-
-print(f"{first_name}{delimiter}{second_name}")
--- Python-Fundamentals/Labs-And-Homeworks/Data-Types-and-Variables/convert_meters_to_kilometers.py
+++
@@ -1,4 +0,0 @@
-distance_in_meters = int(input())
-distance_in_kilometers = distance_in_meters / 1000
-
-print(f"{distance_in_kilometers:.2f}")
