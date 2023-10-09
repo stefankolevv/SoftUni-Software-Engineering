9aafee9797cc2d53be70cfd9ec95cfca15f464e0
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-09 12:05:33 +0300

    Create factorial_division.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/Functions/factorial_division.py
@@ -0,0 +1,12 @@
+def calculate_the_factorial(number):
+    for current_number in range(1, number):
+        number *= current_number
+    return number  # initial number factorial -> number!
+
+
+first_number = int(input())
+second_number = int(input())
+first_number_factorial = calculate_the_factorial(first_number)
+second_number_factorial = calculate_the_factorial(second_number)
+result = first_number_factorial / second_number_factorial
+print(f"{result:.2f}")
