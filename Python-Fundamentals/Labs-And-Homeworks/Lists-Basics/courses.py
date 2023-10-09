dae35d12e22a56bc1fdf1d8cb0ebf08c1af584a6
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-09 12:28:13 +0300

    Delete Python-Fundamentals/Labs-And-Homeworks/Lists-Basics directory

--- Python-Fundamentals/Labs-And-Homeworks/Lists-Basics/courses.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-courses_list = []
-
-for _ in range(n):
-    course_name = (input())
-    courses_list.append(course_name)
-
-print(courses_list)
--- Python-Fundamentals/Labs-And-Homeworks/Lists-Basics/invert_values.py
+++
@@ -1,8 +0,0 @@
-list_with_numbers = input().split()
-opposite_numbers = []
-
-for number in list_with_numbers:
-    current_number = -int(number)
-    opposite_numbers.append(current_number)
-
-print(opposite_numbers)
