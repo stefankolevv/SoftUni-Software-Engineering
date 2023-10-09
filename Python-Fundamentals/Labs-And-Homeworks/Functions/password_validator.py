51aea7cd0ee7f712beb90f3936109e18fb93ee8c
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-09 12:13:42 +0300

    Create password_validator.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/Functions/password_validator.py
@@ -0,0 +1,21 @@
+def password_validator(some_password: str) -> list:
+    list_of_errors = []
+    if len(some_password) < 6 or len(some_password) > 10:
+        list_of_errors.append("Password must be between 6 and 10 characters")
+    if not some_password.isalnum():
+        list_of_errors.append("Password must consist only of letters and digits")
+    digits_counter = 0
+    for character in some_password:
+        if character.isdigit():
+            digits_counter += 1
+    if digits_counter < 2:
+        list_of_errors.append("Password must have at least 2 digits")
+    return list_of_errors
+
+
+password = input()
+errors_in_password = password_validator(password)
+if not errors_in_password:
+    print("Password is valid")
+else:
+    print("\n".join(errors_in_password))
