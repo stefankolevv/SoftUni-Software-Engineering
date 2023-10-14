4a2832ba0e21da224db20e988d7bc6f98972e28e
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-14 10:59:09 +0300

    Create group_of_10_s.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/group_of_10_s.py
@@ -0,0 +1,7 @@
+numbers = [int(number) for number in input().split(", ")]
+current_group = 10
+while numbers:
+    filtered_numbers_for_current_group = [number for number in numbers if number <= current_group]
+    print(f"Group of {current_group}'s: {filtered_numbers_for_current_group}")
+    current_group += 10
+    numbers = [number for number in numbers if number not in filtered_numbers_for_current_group]
