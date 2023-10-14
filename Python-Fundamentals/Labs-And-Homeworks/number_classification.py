583629b6a40106e7e62a356a533a9c455ac84fce
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-14 10:51:59 +0300

    Create number_classification.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/number_classification.py
@@ -0,0 +1,21 @@
+def positive_numbers(list_of_numbers):
+    return ', '.join([number for number in list_of_numbers if int(number) >= 0])
+ 
+ 
+def negative_numbers(list_of_numbers):
+    return ', '.join([number for number in list_of_numbers if int(number) < 0])
+ 
+ 
+def even_numbers(list_of_numbers):
+    return ', '.join([number for number in list_of_numbers if int(number) % 2 == 0])
+ 
+ 
+def odd_numbers(list_of_numbers):
+    return ', '.join([number for number in list_of_numbers if int(number) % 2 != 0])
+ 
+ 
+numbers = input().split(", ")
+print(f"Positive: {positive_numbers(numbers)}")
+print(f"Negative: {negative_numbers(numbers)}")
+print(f"Even: {even_numbers(numbers)}")
+print(f"Odd: {odd_numbers(numbers)}")
