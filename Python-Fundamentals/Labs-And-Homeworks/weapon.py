c2a9589ebc56c26b94c2640c2b1d8569f0e3c432
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-10-28 14:02:34 +0300

    Create weapon.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/weapon.py
@@ -0,0 +1,12 @@
+class Weapon:
+    def __init__(self, bullets: int):
+        self.bullets = bullets
+
+    def shoot(self):
+        if self.bullets > 0:
+            self.bullets -= 1
+            return "shooting..."
+        return "no bullets left"
+
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
