0258dcab578fad31d4cadd8971e911481486224f
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-11-05 10:47:07 +0200

    Create company_users.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/company_users.py
@@ -0,0 +1,18 @@
+companies = {}
+
+while True:
+    command = input()
+    if command == "End":
+        break
+
+    company, employee_id = command.split(" -> ")
+
+    if company not in companies:
+        companies[company] = set()
+
+    companies[company].add(employee_id)
+
+for company, employees in companies.items():
+    print(company)
+    for employee in employees:
+        print(f"-- {employee}")
