058f3d19d5ba2c63f4f2a0d54c5da5b7b00b2325
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-11-11 14:34:16 +0200

    Create search.py

---
+++ Python-Fundamentals/Labs-And-Homeworks/Lists-Basics/search.py
@@ -0,0 +1,18 @@
+n = int(input())
+magic_word = input()
+
+strings = []
+
+for _ in range(n):
+    string = input()
+    strings.append(string)
+
+print(strings)
+
+filtered_strings = []
+
+for current_string in strings:
+    if magic_word in current_string:
+        filtered_strings.append(current_string)
+
+print(filtered_strings)
