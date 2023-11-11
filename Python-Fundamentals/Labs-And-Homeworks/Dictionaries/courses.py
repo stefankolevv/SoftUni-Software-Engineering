cab05538cc902c93af2c92c6384cec79a6038a40
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2023-11-11 14:10:30 +0200

    Update courses.py

--- Python-Fundamentals/Labs-And-Homeworks/Dictionaries/courses.py
+++ Python-Fundamentals/Labs-And-Homeworks/Dictionaries/courses.py
@@ -1,8 +1,20 @@
-n = int(input())
-courses_list = []
+courses = {}
 
-for _ in range(n):
-    course_name = (input())
-    courses_list.append(course_name)
+while True:
+    input_line = input()
+    if input_line == "end":
+        break
 
-print(courses_list)
+    course_name, student_name = input_line.split(" : ")
+
+    if course_name not in courses:
+        courses[course_name] = []
+
+    courses[course_name].append(student_name)
+
+sorted_courses = dict(sorted(courses.items(), key=lambda x: len(x[1]), reverse=True))
+
+for course, students in sorted_courses.items():
+    print(f"{course}: {len(students)}")
+    for student in sorted(students):
+        print(f"-- {student}")
