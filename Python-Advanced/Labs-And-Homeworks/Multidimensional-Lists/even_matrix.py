bdb298b65deabffde7e2c1304a238c35403ce06e
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-01-19 00:03:14 +0200

    Create even_matrix.py

---
+++ Python-Advanced/Labs-And-Homeworks/Multidimensional-Lists/even_matrix.py
@@ -0,0 +1,17 @@
+row_count = int(input())
+
+matrix = []
+
+for i in range(row_count):
+    data = [int(el) for el in input().split(", ") if int(el) % 2 == 0]
+    matrix.append(data)
+
+print(matrix)
+even_matrix = []
+
+for row in matrix:
+     sub_list = []
+     for el in row:
+         if el % 2 == 0:
+             sub_list.append(el)
+             even_matrix.append(sub_list)
