d25e41c5e0259c29678f64f51d9306c1945a378f
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-01-20 21:29:57 +0200

    Create snake_moves.py

---
+++ Python-Advanced/Labs-And-Homeworks/Multidimensional-Lists/snake_moves.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+rows, cols = [int(x) for x in input().split()]
+word = list(input())
+
+word_copy = deque(word)
+multiplier = 1
+
+for row in range(rows):
+    while len(word_copy) < cols:
+        word_copy.extend(word)
+
+    print(*[word_copy.popleft() for _ in range(cols)][::multiplier], sep="")
+    multiplier *= -1
