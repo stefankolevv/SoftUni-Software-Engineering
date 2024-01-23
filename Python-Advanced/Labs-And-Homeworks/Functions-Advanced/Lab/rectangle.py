a4e09d58d82039f7d83b1822a1299e29b981346e
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-01-23 23:49:19 +0200

    Create rectangle.py

---
+++ Python-Advanced/Labs-And-Homeworks/Functions-Advanced/Lab/rectangle.py
@@ -0,0 +1,11 @@
+def rectangle(length, width):
+    if not isinstance(length, int) or not isinstance(width, int):
+        return 'Enter valid values!'
+
+    def area():
+        return length * width
+
+    def perimeter():
+        return 2*(length + width)
+
+    return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
