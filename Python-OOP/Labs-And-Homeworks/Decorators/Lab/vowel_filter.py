a2482eff90524122ad7fd38d9c421e53d4d1c9a7
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-03-19 23:52:38 +0200

    Add files via upload

---
+++ Python-OOP/Labs-And-Homeworks/Decorators/Lab/vowel_filter.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+
+def vowel_filter(function):
+    def wrapper(*args, **kwargs):
+        result = function(*args, **kwargs)
+        return [el for el in result if el.lower() in 'aeyoui']
+    return wrapper
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
