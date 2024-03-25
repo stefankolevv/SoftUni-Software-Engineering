ee1a6809640910830deeedd8e9ebe45334e7cba3
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-03-25 21:22:47 +0200

    Delete Python-Advanced directory

--- Python-Advanced/Labs-And-Homeworks/Multidimensional-Lists/Lab/flattening_matrix.py
+++
@@ -1,7 +0,0 @@
-row = int(input())
-
-flatten = []
-for i in range(row):
-    row_data = [int(el) for el in input().split(", ")]
-    flatten.extend(row_data)
-print(flatten)
--- Python-Advanced/Labs-And-Homeworks/Tuples-and-Sets/Exercise/count_symbols.py
+++
@@ -1,4 +0,0 @@
-text = input()
-
-for symbol in sorted(set(text)):
-    print(f"{symbol}: {text.count(symbol)} time/s")
