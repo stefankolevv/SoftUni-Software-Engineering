a3655edff965e969223bfa25833f797042067aec
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-05-15 19:27:50 +0300

    Tasks solutions

---
+++ Python-Advanced/Python-OOP/Labs-And-Homeworks/Polymorphism-and-Abstraction/Exercise/Wild-Farm/project/test.py
@@ -0,0 +1,27 @@
+# test code 1
+from project.animals.birds import Owl
+from project.food import Meat, Vegetable
+
+owl = Owl("Pip", 10, 10)
+print(owl)
+meat = Meat(4)
+print(owl.make_sound())
+owl.feed(meat)
+veg = Vegetable(1)
+print(owl.feed(veg))
+print(owl)
+
+# test code 2
+from project.animals.birds import Hen
+from project.food import Meat, Vegetable, Fruit
+
+hen = Hen("Harry", 10, 10)
+veg = Vegetable(3)
+fruit = Fruit(5)
+meat = Meat(1)
+print(hen)
+print(hen.make_sound())
+hen.feed(veg)
+hen.feed(fruit)
+hen.feed(meat)
+print(hen)
