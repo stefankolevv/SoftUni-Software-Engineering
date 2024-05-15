e65783ed07b3bbe3347f699bf968f9032a561712
Stefan Kolev <stefi.kolev.bg14@gmail.com>
2024-05-15 11:33:00 +0300

    Delete Python-Advanced/Python-OOP/Exam-6-April-2024 directory

--- Python-Advanced/Python-OOP/Exam-6-April-2024/project/campaigns/high_budget_campaign.py
+++
@@ -1,8 +0,0 @@
-from campaigns.base_campaign import BaseCampaign
-
-class HighBudgetCampaign(BaseCampaign):
-    def __init__(self, campaign_id: int, brand: str, required_engagement: float):
-        super().__init__(campaign_id, brand, 5000.0, required_engagement)
-
-    def check_eligibility(self, engagement_rate: float):
-        return engagement_rate >= 1.2 * self.required_engagement
--- Python-Advanced/Python-OOP/Exam-6-April-2024/project/campaigns/low_budget_campaign.py
+++
@@ -1,8 +0,0 @@
-from campaigns.base_campaign import BaseCampaign
-
-class LowBudgetCampaign(BaseCampaign):
-    def __init__(self, campaign_id: int, brand: str, required_engagement: float):
-        super().__init__(campaign_id, brand, 2500.0, required_engagement)
-
-    def check_eligibility(self, engagement_rate: float):
-        return engagement_rate >= 0.9 * self.required_engagement
